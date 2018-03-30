bc94dd4a51b78a52c8d32301a17883fe4192ab0a
stuFireball <stuar_hahn@outlook.com>
2018-03-30 14:53:35 +0800

    Working store config data-formatting correction

--- src/main.cpp
+++ src/main.cpp
@@ -18,29 +18,29 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
-    int numCams = 0;    //number of cameras connected
-    Config Config1;	//decleare config class
-    int reading[] = {0, 0, 0, 0, 0, 0, 0}; //int array for holding read from config file
+	int numCams = 0;	//number of cameras connected
+	Config Config1;	//decleare config class
+	int reading[] = {0, 0, 0, 0, 0, 0, 0}; //int array for holding read from config file
 
-    printf("Checking camera connections\n");
-    //for testing, can be removed later
+	printf("Checking camera connections\n");
+	//for testing, can be removed later
 
-    numCams = ASIGetNumOfConnectedCameras();
-    //check the number of connected cameras
+	numCams = ASIGetNumOfConnectedCameras();
+	//check the number of connected cameras
 
-    if (numCams == 1)
-        printf("1 camera has been detected\n");
+	if (numCams == 1)
+		printf("1 camera has been detected\n");
 
-    else
-        printf("%d cameras have been detected\n",numCams);
-    	//prints the number of cameras which have been detected
+	else
+		printf("%d cameras have been detected\n",numCams);
+		//prints the number of cameras which have been detected
 
-    string configLine_;
-    ifstream configFile_ ("camConfig.txt");
+	string configLine_;
+	ifstream configFile_ ("camConfig.txt");
 
-    if(configFile_.is_open())
-    {
-        printf("successfully opened camConfig.txt\n");
+	if(configFile_.is_open())
+	{
+		printf("successfully opened camConfig.txt\n");
 
 		string camParam;
 		int camVal;
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
 		while(configFile_ >> camParam >> camVal && i<7)
 		{
 	 	   reading[i] = camVal;
-		    i++;
+			i++;
 		}
 		configFile_.close();
 
@@ -62,23 +62,23 @@ int main(int argc, char* argv[])
 		Config1.WB_B = reading [4];
 		Config1.Bandwidth = reading [5];
 		Config1.HS_Mode = reading [6];
-    }
+	}
 	
-    else
-        printf("Unable to read camConfig.txt\n");
+	else
+		printf("Unable to read camConfig.txt\n");
 
 	//print to check on the configfile	
 	printConfig(Config1);
 
 
 
-    return 0;
+	return 0;
 }
 
 
 void printConfig(Config& Config1)
 {
-    cout << Config1.Exposure << endl;
+	cout << Config1.Exposure << endl;
 	cout << Config1.Gain << endl;
 	cout << Config1.Gamma << endl;
 	cout << Config1.WB_R << endl;
